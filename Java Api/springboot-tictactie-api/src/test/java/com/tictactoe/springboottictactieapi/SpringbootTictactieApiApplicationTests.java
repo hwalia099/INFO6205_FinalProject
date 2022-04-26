c51d20f63dcaf93fc37ba55fd840fec98247e3e3
iakanksha2908 <99631346+iakanksha2908@users.noreply.github.com>
2022-04-26 19:14:26 -0400

    Demo Test case created

--- Java Api/springboot-tictactie-api/src/test/java/com/tictactoe/springboottictactieapi/SpringbootTictactieApiApplicationTests.java
+++ Java Api/springboot-tictactie-api/src/test/java/com/tictactoe/springboottictactieapi/SpringbootTictactieApiApplicationTests.java
@@ -1,14 +1,28 @@
 package com.tictactoe.springboottictactieapi;
 
+import com.tictactoe.springboottictactieapi.Classes.Play;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.tictactoe.springboottictactieapi.Classes.*;
+
+//import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertTrue;
 
 @SpringBootTest
 class SpringbootTictactieApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-
-	}
+    @Test
+    void shouldReturnMove() {
+        char[][] board = new char[3][3];
+        for(int i = 0; i < 3; i++) {
+            board[i][0] = ' ';
+            board[i][1] = ' ';
+            board[i][2] = ' ';
+        }
+        Play play = new Play(board);
+        PossiblePlay possibleMove= play.computerPlay('o', -1, 4, 0);
+        assert possibleMove.getCol() >= 0;
+        assert possibleMove.getRow() >= 0;
+    }
 
 }
